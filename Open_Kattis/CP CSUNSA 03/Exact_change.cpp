fee14f2a01f06e2cd5cd9be4aebfdabc7e9ebd2b
UbertoGC <84751424+UbertoGC@users.noreply.github.com>
2022-10-31 16:15:58 -0500

    Update Exact_change.cpp

--- Open_Kattis/CP CSUNSA 03/Exact_change.cpp
+++ Open_Kattis/CP CSUNSA 03/Exact_change.cpp
@@ -1,35 +1,29 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
-int billetes[100];
-int limite_valor;
-int limite_cant;
-void calculo(int n, int objetivo, int pos=0,int cant=0){
-    if(objetivo<=0){
-        if(objetivo>limite_valor){
-            limite_valor=objetivo;
-            limite_cant=cant;
-        }
-        else if(objetivo==limite_valor){
-            if(cant<limite_cant)
-                limite_cant=cant;
-        }
-    }
-    else{
-        for (int i = pos; i < n; i++)
-            calculo(n,objetivo-billetes[i],i+1,cant+1);
-    }
-}
+int inf = 2 << 28;
 int main() {
-	int t,p,n;
-    cin>>t;
-    while (t--){
-        cin>>p>>n;
-        limite_valor=INT16_MIN;
-        limite_cant=INT16_MAX;
-        for (int i = 0; i < n; i++){
-            cin>>billetes[i];
+    int cases;
+    cin >> cases;
+    while(cases--) {
+        int n;
+        cin >> n;
+        vector<int> v(10001,inf);
+        v[0] = 0;
+        int k;
+        cin >> k;
+        for(int i = 0; i < k; i++) {
+            int coin;
+            cin >> coin;
+            for(int i = 10000 - coin; i >= 0; i--) {
+                v[i+coin] = min(v[i+coin], v[i]+1);
+            }
         }
-        calculo(n,p);
-        cout<<p+(limite_valor*-1)<<" "<<limite_cant<<endl;
+        for(int i = n; i <= 10000; i++) {
+            if(v[i] < inf/2) {
+                cout << i << " " << v[i] << endl;
+                break;
+            }
+        }
     }
 }
